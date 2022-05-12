3619fdc910aa12eb03903758321204c55ccc15b5
Mario Tepe <marioandresth@yahoo.com>
2022-05-11 21:36:17 -0600

    first commit

---
+++ src/main/kotlin/com/tepe/currencyproject/CurrencyprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.tepe.currencyproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurrencyprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/tepe/currencyproject/CurrencyprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tepe.currencyproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
